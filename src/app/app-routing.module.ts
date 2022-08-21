6d1e2e605db5a76540c42768bed5e31b1a62e14b
Miguel Gutierrez <miguelepst@hotmail.com>
2022-08-21 11:56:41 -0500

    Ruting navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { MiguelcodeComponent } from './pages/miguelcode/miguelcode.component';
 
 const routes: Routes = [
+  {path:'', redirectTo: '/products', pathMatch:'full'}, 
   {path:'miguelcode',component: MiguelcodeComponent},
   { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
   { path: 'checkout', loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutModule) },
+  
 
   {path:'**', redirectTo: '', pathMatch:'full'}  // debe ir al final de todo el listado de rutas
 ];
--- src/app/shared/componentes/header/header.component.ts
+++ src/app/shared/componentes/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ShoppingCartService } from './services/shopping-cart.service';
+
 
 @Component({
   selector: 'app-header',
@@ -7,13 +9,20 @@ import { ShoppingCartService } from './services/shopping-cart.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent  {
-quantity$ =this.shoppingCartSvc.quantityAction$;
+
+/* quantity$ =this.shoppingCartSvc.quantityAction$;
 total$ =this.shoppingCartSvc.totalAction$;
 cart$ =this.shoppingCartSvc.cartAction$;
 
   constructor(private shoppingCartSvc: ShoppingCartService) { }
 
   ngOnInit(): void {
+  } */
+
+  constructor(private router: Router) { }
+
+  goToCheckout(): void {
+    this.router.navigate(['/checkout']);
   }
 
 }
